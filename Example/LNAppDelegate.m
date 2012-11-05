767b3c8670368b425aa9c27234d4b24193691d74
David Keegan <git@davidkeegan.com>
2012-11-05 00:36:46 -0800

    Adding an example of how to use REComposeViewController to post messages

--- Example/LNAppDelegate.m
+++ Example/LNAppDelegate.m
@@ -8,11 +8,14 @@
 
 #import "LNAppDelegate.h"
 #import "LessNeglect.h"
+#import "REComposeViewController.h"
 
 @implementation LNAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
     [[LNManager sharedInstance] setCode:@"<code>" andSecret:@"<secret>"];
+    [[LNManager sharedInstance] setLogEvents:YES]; // for debugging
+
     LNPerson *currentPerson = [LNPerson personWithName:@"Christopher Gooley" andEmail:@"gooley@lessneglect.com"];
     currentPerson.properties = @{LNPersonPropertyAvatarURL: @"https://foliohd.com/image/sqavatar/gooley.jpg"};
     [[LNManager sharedInstance] setCurrentPerson:currentPerson];
@@ -34,8 +37,33 @@
 
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = [[UIViewController alloc] init];
+
+    CGRect buttonRect = CGRectZero;
+    buttonRect.size = CGSizeMake(200, 44);
+    buttonRect.origin.x = round(CGRectGetMidX([[UIScreen mainScreen] bounds])-CGRectGetMidX(buttonRect));
+    buttonRect.origin.y = round(CGRectGetMidY([[UIScreen mainScreen] bounds])-CGRectGetMidY(buttonRect));
+    UIButton *messageButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [messageButton setTitle:@"Send Feedback..." forState:UIControlStateNormal];
+    [messageButton addTarget:self action:@selector(messageAction:) forControlEvents:UIControlEventTouchUpInside];
+    messageButton.frame = buttonRect;
+    [self.window.rootViewController.view addSubview:messageButton];
+    
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+- (void)messageAction:(id)sender{
+    REComposeViewController *composeViewController = [[REComposeViewController alloc] init];
+    composeViewController.title = @"Feedback";
+    composeViewController.text = @"Hi there!";
+    composeViewController.completionHandler = ^(REComposeResult result){
+        if(result == REComposeResultPosted){
+            [[LNManager sharedInstance] postEventForCurrentPerson:
+             [LNMessageEvent messageEventWithBody:composeViewController.text
+                                       andSubject:composeViewController.title]];
+        }
+    };
+    [self.window.rootViewController presentViewController:composeViewController animated:YES completion:nil];
 }
 
 @end
